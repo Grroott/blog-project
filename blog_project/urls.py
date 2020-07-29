04c956e56cd2f04290f33884cd0024d404bca246
Gokul nath <gokulnath634@gmail.com>
2020-07-29 09:50:17 +0530

    Created a basic form for registration

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register, name='register'),
     path('', include('blog.urls')),
+    
 ]
 
 if settings.DEBUG:
