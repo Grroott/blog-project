d257ec6fec0ee8a4e9410366b7da009533cde34a
Gokul nath <gokulnath634@gmail.com>
2020-07-29 19:50:12 +0530

    Basic user registration form

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
-
+    
 class UserRegisterForm(UserCreationForm):
 
 	email = forms.EmailField()
@@ -10,3 +12,31 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+	def __init__(self, *args, **kwargs):
+		super(UserRegisterForm, self).__init__(*args, **kwargs)
+		self.fields['password1'].help_text = "Passwords must be of minimun 8 charectors"
+		self.fields['username'].help_text = ""
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+
+		# checking Email unique
+
+		try:
+		    User.objects.get(email=cleaned_data['email'])
+		except User.DoesNotExist:
+		    pass
+		else:
+		    raise ValidationError('This Email address already exists! Try different one!')
+
+		# checking User unique
+
+		try:
+		    User.objects.get(username=cleaned_data['username'])
+		except User.DoesNotExist:
+		    pass
+		else:
+		    raise forms.ValidationError('User already exists! Try different one!')
+
+		return cleaned_data
