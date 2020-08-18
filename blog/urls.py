7e3646176fc9eda5c930db9736acc9160d630963
Gokul nath <gokulnath634@gmail.com>
2020-08-18 20:50:30 +0530

    Highlight searched words

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ urlpatterns = [
     path('', home, name='home'),
     path('search/', search, name='search'),
     path('new-post/', new_post, name='new-post'),
-    path('<slug>/', post_detail, name='post-detail'),
     path('<slug>/bookmark-post/', bookmark_post, name='bookmark-post'),
     path('<slug>/like-post/', like_post, name='like-post'),
     path('<slug>/edit/', edit_post, name='edit-post'),
     path('<slug>/delete/', delete_post, name='delete-post'),
-    path('top-posts', top_posts, name='top-posts'),
+    path('top-posts/', top_posts, name='top-posts'),
+    path('<slug>/', post_detail, name='post-detail'),
     
 ]
